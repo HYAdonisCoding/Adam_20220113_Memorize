0294c3e7f7bce4049b2af2a5011939cc4d497748
Adam <296786475@qq.com>
2022-01-13 15:56:10 +0800

    Initial Commit

---
+++ Adam_20220113_Memorize/Adam_20220113_MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Adam_20220113_MemorizeApp.swift
+//  Adam_20220113_Memorize
+//
+//  Created by Adam on 2022/1/13.
+//
+
+import SwiftUI
+
+@main
+struct Adam_20220113_MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Adam_20220113_Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Adam_20220113_Memorize
+//
+//  Created by Adam on 2022/1/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
